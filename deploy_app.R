ce6a79e5c2387838cc242cc49135ae83d575ad5a
Antti Rask <anttilennartrask@gmail.com>
2025-03-02 20:37:51 +0200

    Added Sys.getenv around the secrets

--- deploy_app.R
+++ deploy_app.R
@@ -1,13 +1,20 @@
 library(here)
 library(rsconnect)
 
-# source(here("app/www/secret.R")) need this only for the local version
+# Uncomment for the local version
+# source(here("app/www/secret.R"))
+# fmt: skip
+# setAccountInfo(
+#   name   = SHINY_APPS_NAME,
+#   token  = SHINY_APPS_TOKEN,
+#   secret = SHINY_APPS_SECRET
+# )
 
 # fmt: skip
 setAccountInfo(
-  name   = SHINY_APPS_NAME,
-  token  = SHINY_APPS_TOKEN,
-  secret = SHINY_APPS_SECRET
+  name   = Sys.getenv(SHINY_APPS_NAME),
+  token  = Sys.getenv(SHINY_APPS_TOKEN),
+  secret = Sys.getenv(SHINY_APPS_SECRET)
 )
 
 # fmt: skip
