8ce0d24b7fbb389054d5474feb3a00afc8cc0f6e
Antti Rask <anttilennartrask@gmail.com>
2025-03-02 19:49:11 +0200

    No need for secret.R in the published version

--- deploy_app.R
+++ deploy_app.R
@@ -1,17 +1,19 @@
 library(here)
 library(rsconnect)
 
-source(here("app/www/secret.R"))
+# source(here("app/www/secret.R")) need this only for the local version
 
+# fmt: skip
 setAccountInfo(
-    name   = SHINY_APPS_NAME,
-    token  = SHINY_APPS_TOKEN,
-    secret = SHINY_APPS_SECRET
+  name   = SHINY_APPS_NAME,
+  token  = SHINY_APPS_TOKEN,
+  secret = SHINY_APPS_SECRET
 )
 
+# fmt: skip
 deployApp(
-    appDir      = here("app/"),
-    appName     = "BiblioStatus",
-    account     = "youcanbeapirate",
-    forceUpdate = TRUE
+  appDir      = here("app/"),
+  appName     = "BiblioStatus",
+  account     = "youcanbeapirate",
+  forceUpdate = TRUE
 )
